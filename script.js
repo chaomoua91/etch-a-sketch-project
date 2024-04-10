4c5af7364b9e54065ec010d645c3d96bcfe85397
Chao Moua <chaomoua91@gmail.com>
2024-04-09 19:10:44 -0500

    styling the pages

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-const gridSide = 600;
-let rows = 16;
-let cols = 16;
+document.addEventListener("DOMContentLoaded", function () {
+    createBoard(16);
+    console.log ("hi");
+}
+)
 
-const sketchArea = document.querySelector("#sketch-area");
-sketchArea.style.width = sketchArea.style.height = `${gridSide}px`;
+function createBoard (size) {
+    let board = document.querySelector("#sketch-area");
 
-function changeBackgroundColor () {
-    this.style.backgroundColor = "orange";
+    board.style.gridTemplateColumns = `repeat (${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat (${size}, 1fr)`;
 }
 
 
-function createGridCells () {
-    for (let i = 0; i < (rows * cols); i++) {
-        const gridCell = document.createElement("div");
 
-        gridCell.style.width =  `${(gridSide / cols) - 2}px`;
-        gridCell.style.height = `${(gridSide / rows) - 2}px`;
 
-        gridCell.classList.add("cell");
-
-        sketchArea.appendChild(gridCell);
-
-        gridCell.addEventListener("mouseover", changeBackgroundColor); 
+function getSize () {
+    let input = prompt ("Select the size of the drawing grid.");
+    let message = document.querySelector("#message");
+    if (input === "") {
+        message.innerHTML = "Please provide a number";
+    } else if (input < 0 || input > 100) {
+        message.innerHTML = "Provide a number between 1 and 100"
+    } else {
+        message.innerHTML = "Let's Draw!"
+        return input;
     }
-
 }
-
-createGridCells();
